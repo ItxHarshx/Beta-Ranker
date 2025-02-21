eaa03c81bc3eba89558e2bfb367626165bd5101b
ItxHarshx <itxharshx@gmail.com>
2025-02-21 22:32:25 +0530

    Update database.py

--- database.py
+++ database.py
@@ -10,7 +10,7 @@ async def create_user_if_not_exists(user_id):
     conn = await connect_db()
     await conn.execute(
         """INSERT INTO users (user_id, health, gold_coins, exp, level, essence, last_message_time, last_checkin)
-           VALUES ($1, 100, 0, 0, 1, 0, NOW(), NONE) 
+           VALUES ($1, 100, 0, 0, 1, 0, NOW(), NULL) 
            ON CONFLICT (user_id) DO NOTHING""",
         user_id
     )
