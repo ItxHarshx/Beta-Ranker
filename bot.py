56173f6f8c0900946cea4cc70812d3387f0591fd
ItxHarshx <itxharshx@gmail.com>
2025-02-21 16:02:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,15 +7,14 @@ from aiogram.types import Message
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.filters import Command
 from dotenv import load_dotenv
-from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
 
 # Load environment variables (Bot Token)
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 
 # Initialize bot and dispatcher
-bot = Bot(token=TOKEN,
-parse_mode=ParseMode.HTML)
+bot = Bot(token="YOUR_BOT_TOKEN", default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher()
 
 # Start command
