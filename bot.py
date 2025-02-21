a59508b222ffcf6619cb02eed65d3832aad64220
ItxHarshx <itxharshx@gmail.com>
2025-02-21 14:58:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import logging
 import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import Message
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.filters import Command
 from dotenv import load_dotenv
 
@@ -15,10 +16,23 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 # Start command
+IMAGE_FILE_ID = "https://ibb.co/99h957S4"
+KAISEN_GROUP_LINK = "https://t.me/KaisenWorld"
+
 @dp.message(Command("start"))
 async def start_command(message: Message):
-    await message.answer("🔥 Welcome to Kaisen Ranking Bot! Type /help to see commands.")
-
+    # Create an inline button linking to Kaisen World
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text="Join Chat Group, url=KAISEN_GROUP_LINK)]
+        ]
+    )
+    await message.answer_photo(
+        IMAGE_FILE_ID, 
+        caption="Hey User, Welcome to Kaisen Ranking Bot ! 🎉\n\n📜 **ʜᴏᴡ ᴛᴏ ᴇᴀʀɴ ᴇssᴇɴᴄᴇ ?**\n- ᴊᴜsᴛ ᴄʜᴀᴛ ɪɴ ᴛʜᴇ ɢʀᴏᴜᴘ! ᴇᴠᴇʀʏ ᴍᴇssᴀɢᴇ ʏᴏᴜ sᴇɴᴅ ɢᴇᴛs ʏᴏᴜ ᴄʟᴏsᴇʀ ᴛᴏ ᴇᴀʀɴɪɴɢ ᴇssᴇɴᴄᴇ.\n\nGet started now ! type /help for more commands.",
+        reply_markup=keyboard  # Add the button below the image
+    )
+    
 # Help command
 @dp.message(Command("help"))
 async def help_command(message: Message):
