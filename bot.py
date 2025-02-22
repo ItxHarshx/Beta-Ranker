1a94fa673aebe575bb878df3ec68974f2a68ecda
ItxHarshx <itxharshx@gmail.com>
2025-02-22 10:24:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,9 +7,9 @@ from aiogram.types import Message
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.filters import Command
 from dotenv import load_dotenv
-from features import user_profile, leveling, checkin
+from features import user_profile, leveling, daily
 from features.user_profile import router as profile_router
-from features.checkin import router as checkin_router
+from features.daily import router as daily_router
 from aiogram.enums.parse_mode import ParseMode 
 from aiogram.client.default import DefaultBotProperties
 
@@ -67,7 +67,7 @@ async def help_command(message: Message):
 
 dp.include_router(user_profile.router)
 dp.include_router(leveling.router)
-dp.include_router(checkin_router)
+dp.include_router(daily.router)
 
 # Error handling
 async def main():
