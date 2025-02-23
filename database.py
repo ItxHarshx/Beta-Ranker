1a0f6b2bd0012e975b84212c53e6890680b9425b
ItxHarshx <itxharshx@gmail.com>
2025-02-23 10:28:20 +0530

    Update database.py

--- database.py
+++ database.py
@@ -6,13 +6,13 @@ async def connect_db():
     return await asyncpg.connect("postgresql://postgres:ZOfBatiHLaKonRFXSaycgkmxwSBEKffw@yamanote.proxy.rlwy.net:41047/railway")  # Change credentials!
 
 # Create user if not exists
-async def create_user_if_not_exists(user_id):
+async def create_user_if_not_exists(user_id, first_name):
     conn = await connect_db()
     await conn.execute(
-        """INSERT INTO users (user_id, health, gold_coins, exp, level, essence, last_message_time, last_checkin)
-           VALUES ($1, 100, 0, 0, 1, 0, NOW(), NULL) 
-           ON CONFLICT (user_id) DO NOTHING""",
-        user_id
+        """INSERT INTO users (user_id, first_name, health, gold_coins, exp, level, essence, last_message_time, last_checkin)
+           VALUES ($1, $2, 100, 0, 0, 1, 0, NOW(), NULL) 
+           ON CONFLICT (user_id) DO UPDATE SET first_name = EXCLUDED.first_name""",
+        user_id, first_name
     )
     await conn.close()
 
